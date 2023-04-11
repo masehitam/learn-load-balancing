308b6dc870aaf570326f3b9b9a1410eab4fbdec2
Wiwid Widyanto <wiwid.widyanto7@gmail.com>
2023-04-11 10:07:25 +0700

    initial commit

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask
+import socket
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():
+    return f"Hello, World! test {socket.gethostname()}"
+
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
